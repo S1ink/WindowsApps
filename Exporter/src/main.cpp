19087e91ff6d41d526c95544927b51896d2f2424
Sam Richter <HoodieB2005@outlook.com>
2021-11-21 14:25:36 -0600

    Update

--- Exporter/src/main.cpp
+++ Exporter/src/main.cpp
@@ -3,179 +3,260 @@
 #include <string>
 #include <sstream>
 #include <stdio.h>
-#include <chrono>
+#include <filesystem>
 
-#define ROBOTICS
+#include "Source/basic.h"
+#include "Source/timing.h"
+#include "Source/resources.h"
 
-constexpr char newline = '\n';
-constexpr char space = ' ';
-constexpr char null = '\0';
+namespace stdf = std::filesystem;
+stdf::copy_options copy_settings = stdf::copy_options::recursive | stdf::copy_options::update_existing;
 
-typedef unsigned int uint;
-typedef unsigned long long udlong;
+//#define ROBOTICS
+//#define TESTING
 
-char clearEnd(std::string& str) {
-	char ret = null;
-	udlong len = str.length();
-	for (uint i = 1; i < 3; i++) {
-		if (str[len - i] == 13 || str[len - i] == 10) {
-			ret = str[len - i];
-			str.pop_back();
-		}
-	}
-	return ret;
-}
+//int roboCopy(std::ostream& out, const char* source, const char* destination, const char* options = "/MIR") {
+//	std::ostringstream command;
+//	command << "robocopy \"" << source << "\" \"" << destination << "\" " << options;
+//	/*out << command.str().c_str() << newline;
+//	return 0;*/
+//	FILE* pipe = _popen(command.str().c_str(), "r");
+//	if (!pipe) {
+//		out << "Failed to run robocopy";
+//	}
+//	else {
+//		char buffer[128];
+//		while (fgets(buffer, 128, pipe) != NULL) {
+//			out << buffer;
+//		}
+//	}
+//	return _pclose(pipe);
+//}
 
-int roboCopy(std::ostream& out, const char* source, const char* destination, const char* options = "/MIR") {
-	std::ostringstream command;
-	command << "robocopy \"" << source << "\" \"" << destination << "\" " << options;
-	//out << command.str().c_str() << newline;
-	FILE* pipe = _popen(command.str().c_str(), "r");
-	if (!pipe) {
-		out << "Failed to run robocopy";
-	}
-	else {
-		char buffer[128];
-		while (fgets(buffer, 128, pipe) != NULL) {
-			out << buffer;
-		}
-	}
-	return _pclose(pipe);
-}
+//// 0 -> success
+//// -1 -> file could not be opened
+//// -2 -> incorrect format
+//// -3 -> robocopy error
+//int parse(const char* instructionpath = ".//export.csv") {
+//	std::ifstream reader(instructionpath);
+//	if (reader.is_open()) {
+//
+//		std::string buffer;
+//		char dlm;
+//		
+//		std::getline(reader, buffer);
+//		if ((dlm = clearEnd(buffer)) == null) {
+//			dlm = newline;
+//		}
+//
+//		if ((buffer != "name,source,destination,options") && (buffer != "name,source,destination")) {
+//			std::cout << "CSV file is not in the correct format." << newline;
+//			return -2;
+//		}
+//
+//		std::string name, source, destination, options;
+//		while (std::getline(reader, buffer, dlm)) {
+//			std::istringstream stream(buffer);
+//			std::getline(stream, name, ',');
+//			std::getline(stream, source, ',');
+//			std::getline(stream, destination, ',');
+//			if (std::getline(stream, options)) {
+//				if (!options.compare(0, 8, "ROBOTICS")) {
+//					std::string src, dest;
+//					src.append("/MIR /XD \"").append(source).append("\\build*\"");
+//					roboCopy(std::cout, source.c_str(), destination.c_str(), src.c_str());
+//					src.clear();
+//					std::cout << "{" << name << "}: completed mirroring attempt from " << source << " to " << destination << newline;
+//
+//					src.append(source).append("\\build\\debug");
+//					dest.append(destination).append("\\build\\debug");
+//					roboCopy(std::cout, src.c_str(), dest.c_str());
+//					std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
+//
+//					src.erase(src.end() - 13, src.end());
+//					src.append("\\build");
+//					dest.erase(dest.end() - 13, dest.end());
+//					dest.append("\\build");
+//					roboCopy(std::cout, src.c_str(), dest.c_str(), "vscodeconfig.json /MIR");
+//					std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
+//
+//				}
+//				else {
+//					roboCopy(std::cout, source.c_str(), destination.c_str(), options.c_str());
+//					std::cout << "{" << name << "}: copied " << source << " to " << destination << newline;
+//				}
+//			}
+//			else {
+//				roboCopy(std::cout, source.c_str(), destination.c_str());
+//				std::cout << "{" << name << "}: copied " << source << " to " << destination << newline;
+//			}
+//		}
+//		return 0;
+//	}
+//	std::cout << "File does not exist - change argument, or create file in default location" << newline;
+//	return -1;
+//}
 
-// 0 -> success
-// -1 -> file could not be opened
-// -2 -> incorrect format
-// -3 -> robocopy error
-int parse(const char* instructionpath = ".//export.csv") {
+int parse2(const char* instructionpath = ".//export.csv") {
 	std::ifstream reader(instructionpath);
 	if (reader.is_open()) {
+
+		//std::cout << std::string((std::istreambuf_iterator<char>(reader)), (std::istreambuf_iterator<char>())) << "\n\n";
+
 		std::string buffer;
 		char dlm;
-		
+
 		std::getline(reader, buffer);
 		if ((dlm = clearEnd(buffer)) == null) {
 			dlm = newline;
 		}
 
 		if ((buffer != "name,source,destination,options") && (buffer != "name,source,destination")) {
-			std::cout << "CSV file is not in the correct format." << newline;
+			std::cout << "CSV file is not in the correct format - header must begin with: \"name,source,destination\"\n";
 			return -2;
 		}
+
+		std::string name, source, destination, options;
 		while (std::getline(reader, buffer, dlm)) {
 			std::istringstream stream(buffer);
-			std::string name, source, destination, options;
 			std::getline(stream, name, ',');
 			std::getline(stream, source, ',');
 			std::getline(stream, destination, ',');
 			if (std::getline(stream, options)) {
-				roboCopy(std::cout, source.c_str(), destination.c_str(), options.c_str());
-				std::cout << "{" << name << "}: copied " << source << " to " << destination << newline;
-			}
-			else {
-				roboCopy(std::cout, source.c_str(), destination.c_str());
-				std::cout << "{" << name << "}: copied " << source << " to " << destination << newline;
-			}
-		}
-		return 0;
-	}
-	std::cout << "File does not exist - change argument, or create file in default location" << newline;
-	return -1;
-}
-
-int parseRobotics(const char* instructionpath = ".//projects.csv") {
-	std::ifstream reader(instructionpath);
-	if (reader.is_open()) {
-		std::string buffer;
-		char dlm;
-
-		std::getline(reader, buffer);
-		if ((dlm = clearEnd(buffer)) == null) {
-			dlm = newline;
-		}
-
-		if ((buffer != "proj,psrc,pdest,opt") && (buffer != "proj,psrc,pdest")) {
-			std::cout << "CSV file is not in the correct format." << newline;
-			return -2;
-		}
-		std::string items[4] = { "/.vscode", "/.wpilib", "/src", "/vendordeps" };
-		uint dirs = (sizeof(items) / sizeof(*items));
-		std::string src, dest;
-		while (std::getline(reader, buffer, dlm)) {
-			std::istringstream stream(buffer);
-			std::string name, source, destination, options;
-			std::getline(stream, name, ',');
-			std::getline(stream, source, ',');
-			std::getline(stream, destination, ',');
-			if (std::getline(stream, options)) {
-				if (options.substr(0, 5) == "BASIC") {
-					roboCopy(std::cout, source.c_str(), destination.c_str());
-					std::cout << "{" << name << "}: completed mirroring attempt from " << source << " to " << destination << newline;
+				if (!options.compare(0, 8, "ROBOTICS")) {
+					const std::filesystem::path src(source), dest(destination), build(src/"build");
+					for (std::filesystem::directory_entry const& dir_entry : std::filesystem::directory_iterator{ src }) {
+						if (dir_entry.path() == build) {
+#ifdef TESTING
+							std::cout << "Theoretical copy from {" << (build / "debug") << "} to {" << ((dest / "build") / "debug") << "}\n";
+							std::cout << "Theoretical file copy from {" << (build / "vscodeconfig.json") << "} to {" << ((dest / "build") / "vscodeconfig.json") << "}\n";
+#else
+							try { stdf::copy((build / "debug"), ((dest / "build") / "debug"), copy_settings); }
+							catch (std::filesystem::filesystem_error& e) { std::cout << e.what() << newline; }
+							try { stdf::copy_file((build / "vscodeconfig.json"), ((dest / "build") / "vscodeconfig.json"), stdf::copy_options::update_existing); }
+							catch (std::filesystem::filesystem_error& e) { std::cout << e.what() << newline; }
+#endif
+						}
+						else if (dir_entry.path().filename() == "bin") { std::cout << "Skipped: " << dir_entry << newline; }
+						else {
+#ifdef TESTING
+							std::cout << "Theoretical copy from {" << dir_entry << "} to {" << (dest / dir_entry.path().filename()) << "}\n";
+#else
+							try { stdf::copy(dir_entry, (dest / dir_entry.path().filename()), copy_settings); }
+							catch (std::filesystem::filesystem_error& e) { std::cout << e.what() << newline; }
+#endif
+						}
+					}
+					std::cout << "{" << name << "}: copied \"" << source << "\" to \"" << destination << "\"\n\n";
 				}
 				else {
-					for (uint i = 0; i < dirs; i++) {
-						src = source + items[i];
-						dest = destination + items[i];
-						roboCopy(std::cout, src.c_str(), dest.c_str(), options.c_str());
-						std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
-					}
+#ifdef TESTING
+					std::cout << "Theoretical copy from {" << stdf::path(source) << "} to {" << stdf::path(destination) << "}\n";
+#else
+					try { stdf::copy(source, destination, copy_settings); }
+					catch (std::filesystem::filesystem_error& e) { std::cout << e.what() << newline; }
+#endif
+					std::cout << "{" << name << "}: copied \"" << source << "\" to \"" << destination << "\" - Options detected but unused\n\n";
 				}
 			}
 			else {
-				for (uint i = 0; i < dirs; i++) {
-					src = source + items[i];
-					dest = destination + items[i];
-					roboCopy(std::cout, src.c_str(), dest.c_str());
-					std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
-				}
+#ifdef TESTING
+				std::cout << "Theoretical copy from {" << stdf::path(source) << "} to {" << stdf::path(destination) << "}\n";
+#else
+				try { stdf::copy(source, destination, copy_settings); }
+				catch (std::filesystem::filesystem_error& e) { std::cout << e.what() << newline; }
+#endif
+				std::cout << "{" << name << "}: copied \"" << source << "\" to \"" << destination << "\"\n\n";
 			}
 		}
 		return 0;
 	}
-	std::cout << "File does not exist - change argument, or create file in default location" << newline;
+	std::cout << "File (" << instructionpath << ") does not exist - change argument, or create file in default location" << newline;
 	return -1;
 }
 
+//int parseRobotics(const char* instructionpath = ".//projects.csv") {
+//	std::ifstream reader(instructionpath);
+//	if (reader.is_open()) {
+//		std::string buffer;
+//		char dlm;
+//
+//		std::getline(reader, buffer);
+//		if ((dlm = clearEnd(buffer)) == null) {
+//			dlm = newline;
+//		}
+//
+//		if ((buffer != "proj,psrc,pdest,opt") && (buffer != "proj,psrc,pdest")) {
+//			std::cout << "CSV file is not in the correct format." << newline;
+//			return -2;
+//		}
+//		const char* items[4] = { "/.vscode", "/.wpilib", "/src", "/vendordeps" };
+//		uint dirs = (sizeof(items) / sizeof(*items));
+//		std::string src, dest, name, source, destination, options;
+//		while (std::getline(reader, buffer, dlm)) {
+//			std::istringstream stream(buffer);
+//			std::getline(stream, name, ',');
+//			std::getline(stream, source, ',');
+//			std::getline(stream, destination, ',');
+//			if (std::getline(stream, options)) {
+//				if (options.substr(0, 5) == "BASIC") {
+//					roboCopy(std::cout, source.c_str(), destination.c_str());
+//					std::cout << "{" << name << "}: completed mirroring attempt from " << source << " to " << destination << newline;
+//				}
+//				else {
+//					for (uint i = 0; i < dirs; i++) {
+//						src = source + items[i];
+//						dest = destination + items[i];
+//						roboCopy(std::cout, src.c_str(), dest.c_str(), options.c_str());
+//						std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
+//					}
+//				}
+//			}
+//			else {
+//				for (uint i = 0; i < dirs; i++) {
+//					src = source + items[i];
+//					dest = destination + items[i];
+//					roboCopy(std::cout, src.c_str(), dest.c_str());
+//					std::cout << "{" << name << "}: completed mirroring attempt from " << src << " to " << dest << newline;
+//				}
+//			}
+//		}
+//		return 0;
+//	}
+//	std::cout << "File does not exist - change argument, or create file in default location" << newline;
+//	return -1;
+//}
+
 int main(int argc, char* argv[]) {	//add timer
-	std::chrono::time_point<std::chrono::high_resolution_clock> start = std::chrono::high_resolution_clock::now();
+	StopWatch runtime("Runtime");
+
 	int ret = 0;
 	if (argc > 1) {
 		std::fstream reader(argv[1]);
 		if (reader.is_open()) {
 #ifdef ROBOTICS
 			ret = parseRobotics(argv[1]);
-#endif
-#ifndef ROBOTICS
-			ret = parse(argv[1]);
+#else
+			ret = parse2(argv[1]);
 #endif
 		}
 		else {
-			std::cout << "Argument {" << argv[1] << "} is not valid or file does not exist" << newline;
-			std::cout << "Usage: ~path~/Exporter.exe PATH/TO/CSV.csv" << newline;
+			std::cout << "Argument {" << argv[1] << "} is not valid or file does not exist\nUsage: ~path~/Exporter.exe PATH/TO/CSV.csv\n";
 			ret = EXIT_FAILURE;
 		}
 	}
 	else {
 #ifdef ROBOTICS
 		ret = parseRobotics();
-#endif
-#ifndef ROBOTICS
-		ret = parse();
+#else
+		ret = parse2();
 #endif
 	}
 
-	std::chrono::duration<double> runtime = std::chrono::high_resolution_clock::now() - start;
-	std::cout << "Runtime: " << runtime.count() << " seconds" << newline;
+	runtime.end();
 
 	std::cin.ignore();
 	exit(ret);
 }
 
-//int main(int argc, char* argv[]) {
-//	if (argc > 1) {
-//		for (int i = 1; i < argc; i++) {
-//			std::cout << argv[i] << newline;
-//		}
-//	}
-//	return 0;
-//}
+//TODO: check if /.gradle is needed -> currently the largest folder
--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -1,26 +1,83 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <thread>
 #include <chrono>
+#include <atomic>
+#include <filesystem>
 
-constexpr char s_dir =
-#ifdef _WIN32
-'\\';
-#elif __unix__
-#elif __linux__
-#else
-'/';
-#endif
+//#include "Source/server/server.h"
+//#include "Source/basic.h"
+#include "Source/resources.h"
+#include "Source/timing.h"
+#include "Source/program.h"
+//#include "Source/sighandle.h"
 
+StopWatch runtime("Runtime", &std::cout, 0);
+
+void callback(std::atomic_bool& external) {
+    std::cin.ignore();
+    external = false;
+}
+
+void on_exit() {
+    runtime.end();
+    std::cin.ignore();
+    std::cin.ignore();
+}
+
+void donut(std::atomic_bool&);
 
 int main(int argc, char* argv[], char* envp[]) {
+    runtime.setStart();
+    progdir.setDir(argv[0]);
+    //sig_handle.setLog(&std::cout);
+
+    //HttpServer server(&std::cout);
+    //server.serve();
+
+    atexit(on_exit);
+
+    /*std::string input;
+    std::cout << "Enter a directory path:\n";
+    std::cin >> input;
+
+    {
+        ScopeTimer execution("Filesystem overhead");
+
+        std::filesystem::path directory(input);
+
+        for (std::filesystem::directory_entry const& dir_entry : std::filesystem::directory_iterator{ directory }) {
+            if (dir_entry.path() == (directory / "obj")) {
+                std::cout << "target directory found\n\n";
+            }
+            else {
+                std::filesystem::path end = dir_entry.path().filename();
+                std::cout << end << newline;
+                std::cout << ("R:\\Rando\\folder" / end) << "\n\n";
+            }
+        }
+    }*/
+
+    std::atomic_bool running = { true };
+
+    std::thread cb(callback, std::ref(running));
+    donut(running);
+    cb.join();
+
+    return 0;
+}
+
+
+
+void donut(std::atomic_bool& external) {
     float A = 0, B = 0;
     float i, j;
     int k;
     float z[1760];
     char b[1760];
     printf("\x1b[2J");
-    for (;;) {
+    while(external) {
         memset(b, 32, 1760);
         memset(z, 0, 7040);
         for (j = 0; j < 6.28; j += 0.07) {
@@ -53,5 +110,6 @@ int main(int argc, char* argv[], char* envp[]) {
             B += 0.00002;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        external = false;
     }
 }
